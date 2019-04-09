4d53db667b170889aa91ae7fadef3dea8c32d6a7
Marzoman5 <46937084+Marzoman5@users.noreply.github.com>
2019-04-09 08:42:10 -0400

    Added Simple_Class This example demonstrates how to make a flasher class. Beginner OOP

---
+++ Simple_Class/Simple_Class.ino
@@ -0,0 +1,17 @@
+#include "Flasher.h"
+
+// create flasher objects
+Flasher led1(12, 100, 400);
+Flasher led2(13, 350, 350);
+
+void setup()
+{
+
+}
+
+void loop()
+{
+  //update the flashers
+  led1.Update();
+  led2.Update();
+}
